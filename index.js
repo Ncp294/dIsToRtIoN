b4983ecd2c0c90139bd1e83d115395f33d79d83b
Nathan Palmer <ncp294@gmail.com>
2026-04-27 16:45:16 -0400

    Addition of log in and simple post on user page.

--- index.js
+++ index.js
@@ -7,8 +7,57 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 
+const { DatabaseSync } = require('node:sqlite');
+const db = new DatabaseSync('database.db', { readonly: false });
+
+db.exec(`
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        password TEXT NOT NULL,
+        username TEXT NOT NULL,
+        name TEXT NOT NULL,
+        posts TEXT DEFAULT (json_array())
+    );
+`);
+
 app.get('/', (req, res) => {
     res.render('index');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/welcome', (req, res) => {
+    let username = req.body['username'];
+    let password = req.body['password'];
+
+    let from = req.get('Referrer');
+
+    if (from.includes('register')) {
+        console.log('registering');
+        let name = req.body['name'];
+
+        db.prepare('INSERT INTO users (username, password, name) VALUES (?, ?, ?)').run(username, password, name);
+    } else if (from.includes('welcome')) {
+        let newPost = req.body['post'];
+        db.prepare('UPDATE users SET posts = json_insert(posts, \'$[#]\', ?) WHERE username = ?').run(newPost, username);
+    }
+
+    let currentUser = db.prepare('SELECT * FROM users WHERE username = ?').get(username);
+    console.log(currentUser);
+
+    if (currentUser.password !== password) {
+        res.send("Wrong password, try again!");
+    } else {
+        res.render('welcome', {
+            'user': currentUser
+        });
+    }
 });
 
 app.listen(port, () => {
