2a607a47fa1f1a5cbb3802a4a3c0808e94ff94a4
Nathan Palmer <ncp294@gmail.com>
2026-04-29 20:11:43 -0400

    Major refactor to organize functions and their communication.

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const path = require('path');
 app.use(express.static('public'));
 app.use(cookieParser());
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 const { DatabaseSync } = require('node:sqlite');
 const db = new DatabaseSync('database.db', { readonly: false });
@@ -40,9 +41,23 @@ app.get('/', (req, res) => {
 
 // send post data to home page and render
 app.get('/home', (req, res) => {
+    const username = req.cookies.username;
+    const password = req.cookies.password;
+    let currentUser = undefined;
+
+    if ( username ){
+        currentUser = db.prepare('SELECT * FROM users WHERE username = ?').get(username);
+    }
+
     let posts = db.prepare('SELECT * FROM posts').all();
+
+    if (!username || !password || password !== currentUser.password) {
+        currentUser = undefined;
+    }
+
     res.render('home', {
-        'posts': posts
+        'posts': posts,
+        'user': currentUser
     });
 });
 
@@ -51,57 +66,51 @@ app.get('/register', (req, res) => {
     res.render('register');
 });
 
-// render login page
-app.get('/login', (req, res) => {
-    res.render('login');
+// register and redirect new users
+app.post('/register', (req, res) => {
+    //TODO: Make username specific so no dupes
+    let username = req.body['username'];
+    let password = req.body['password'];
+    let name = req.body['name'];
+
+    db.prepare('INSERT INTO users (username, password, name) VALUES (?, ?, ?)').run(username, password, name);
+    res.redirect('login');
 });
 
-// render user profile page
-app.post('/welcome', (req, res) => {
+// render login page
+app.get('/login', (req, res) => {
+    let registering = false;
+    const from = req.get('Referrer');
+    console.log(from);
+    if (from.includes('register')) {
+        registering = true;
+    }
+    res.render('login', {
+        "registering": registering
+    });
+});
+
+// verify and redirect login attempts
+app.post('/login', (req, res) => {
     let username = req.body['username'];
     let password = req.body['password'];
 
-    let from = req.get('Referrer');
+    const currentUser = db.prepare('SELECT * FROM users WHERE username = ?').get(username);
 
-    // if coming from registration, add user to db
-    if (from.includes('register')) {
-        //TODO: Make username specific
-        console.log('registering');
-        let name = req.body['name'];
-
-        db.prepare('INSERT INTO users (username, password, name) VALUES (?, ?, ?)').run(username, password, name);
-
-    // if coming from profile page, create post
-    } else if (from.includes('post')) {
-        let newPost = req.body['post'];
-        db.prepare('INSERT INTO posts (content, author) VALUES (?, ?)').run(newPost, username);
-        let post = db.prepare('SELECT * FROM posts WHERE content = ?').get(newPost);
-
-        db.prepare('UPDATE users SET posts = json_insert(posts, \'$[#]\', ?) WHERE username = ?').run(post.id, username);
-    } 
-
-    // get user info from db based on login
-    let currentUser = db.prepare('SELECT * FROM users WHERE username = ?').get(username);
-
-    // if correct password, render profile page
     if (currentUser.password !== password) {
         res.send("Wrong password, try again!");
     } else {
         res.cookie('username', currentUser.username, {maxAge: 3600000});
         res.cookie('password', currentUser.password, {maxAge: 3600000});
-        res.render('welcome', {
-            'user': currentUser
-        });
+        res.redirect('/home');
     }
 });
 
 // render posting page
 app.get('/post', (req, res) => {
+    // TODO: ensure user properly logged in
     const username = req.cookies.username;
     const password = req.cookies.password;
-
-    console.log(username);
-    console.log(password);
 
     const user = {
         username: username,
@@ -111,6 +120,33 @@ app.get('/post', (req, res) => {
     res.render('post', {
         'user': user
     });
+});
+
+app.post('/post', (req, res) => {
+    let username = req.body['username'];
+    let password = req.body['password'];
+
+    let newPost = req.body['post'];
+    db.prepare('INSERT INTO posts (content, author) VALUES (?, ?)').run(newPost, username);
+    let post = db.prepare('SELECT * FROM posts WHERE content = ?').get(newPost);
+
+    db.prepare('UPDATE users SET posts = json_insert(posts, \'$[#]\', ?) WHERE username = ?').run(post.id, username);
+});
+
+app.get('/profile/:username', (req, res) => {
+    const username = req.params.username;
+    const currentUser = db.prepare('SELECT * FROM users WHERE username = ?').get(username);
+    const posts = db.prepare('SELECT * FROM posts WHERE author = ?').all(username);
+    console.log(posts);
+ 
+    if ( !currentUser ) {
+        res.send("User not found. Try again.");
+    } else {
+        res.render('profile', {
+            'user': currentUser,
+            'posts': posts
+        });
+    }
 });
 
 // open app on designated port
---
+++ public/js/login.js
@@ -0,0 +1,6 @@
+const userData = document.getElementById('userData');
+let registering = userData.dataset.registering;
+
+if ( registering == "true" ) {
+    alert("Successfully registered!! Please log in.");
+}
